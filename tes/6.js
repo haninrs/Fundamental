43acb126d2bc7edc15527ac450b242d0f31c7199
haninrs <haninrry@gmail.com>
2024-05-19 23:51:22 +0700

    conflict please

---
+++ tes/6.js
@@ -0,0 +1,14 @@
+// lorem
+
+// i wanana conflict
+
+// mancing keributan
+
+// besok hana presntasi minpro
+
+// ka abid butuh fitur promotor hana
+// tapi hana ga bisa merge githubnya
+
+// ad konflict hahaha
+
+// ini kita tes dulu
