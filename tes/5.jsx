292b87ad6fe113360384322dbd86961508dd1c37
haninrs <haninrry@gmail.com>
2024-05-20 00:00:54 +0700

    tu

--- tes/5.jsx
+++ tes/5.jsx
@@ -6,6 +6,23 @@ export const gtau = () => {
       <h6>thankyiu</h6>
 
       <span>hani btuk</span>
+
+      <table>
+        <th>
+          <td>af</td>
+          <td>asfasf</td>
+          <td>df</td>
+          <td>js</td>
+          <td>
+            <s></s>
+          </td>
+          <td>
+            <a>hlian</a>
+          </td>
+        </th>
+      </table>
     </div>
   );
 };
+
+export default gtau;
